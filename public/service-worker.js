82a2a84f2eb3feee37a18fe05cbcd8a9ce9710db
evanhays98 <evan.hays@epita.fr>
2023-12-17 14:28:41 +0100

    test add notification

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,28 @@
 const CACHE_NAME = 'version-2';
 const urlsToCache = ['index.html', 'offline.html'];
 
+function createNotification() {
+  this.registration.showNotification('Memorix', {
+    body: 'Long time no see! Come back and learn with us!',
+    icon: 'path_to_your_image.png', // Replace with the actual image path
+  });
+}
+
+// Schedule notification every 2 minutes
+function scheduleNotification() {
+  setInterval(
+    () => {
+      createNotification();
+    },
+    2 * 60 * 1000,
+  ); // 2 minutes in milliseconds
+}
+
 this.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('Opened cache');
+      scheduleNotification();
 
       return cache.addAll(urlsToCache);
     }),
